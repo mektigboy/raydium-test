1926c0ee84ecd0f95455953f9a2a47cc5740746e
mektigboy <86902415+mektigboy@users.noreply.github.com>
2024-05-24 01:19:30 -0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use std::str::FromStr;
+
 use anyhow::Result;
+use solana_account_decoder::UiAccountEncoding;
 use solana_client::{
     nonblocking::pubsub_client::PubsubClient,
-    rpc_config::{RpcTransactionLogsConfig, RpcTransactionLogsFilter},
+    rpc_config::{RpcAccountInfoConfig, RpcTransactionLogsConfig, RpcTransactionLogsFilter},
     rpc_response::{Response, RpcLogsResponse},
 };
-use solana_sdk::commitment_config::CommitmentConfig;
+use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey};
 use tokio_stream::StreamExt;
 
 const RAYDIUM_AMM_PROGRAM_V4: &str = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
+const RAYDIUM_LIQUIDITY_POOL: &str = "GH8Ers4yzKR3UKDvgVu8cqJfGzU4cU62mTeg9bcJ7ug6";
 const INITIALIZE_2: &str = "initialize2";
 
 #[tokio::main]
@@ -20,11 +24,9 @@ async fn main() -> Result<()> {
 
     let ws_client = PubsubClient::new(rpc_ws_url.as_str()).await?;
     let (mut stream, _) = ws_client
-        .logs_subscribe(
-            RpcTransactionLogsFilter::Mentions(vec![RAYDIUM_AMM_PROGRAM_V4.to_string()]),
-            RpcTransactionLogsConfig {
-                commitment: Some(CommitmentConfig::finalized()),
-            },
+        .account_subscribe(
+            &Pubkey::from_str(RAYDIUM_LIQUIDITY_POOL).unwrap(),
+            Some(RpcAccountInfoConfig::default()),
         )
         .await?;
 
@@ -33,14 +35,14 @@ async fn main() -> Result<()> {
     while let Some(response) = stream.next().await {
         // dbg!(response.clone());
 
-        // Filter out responses containing errors.
-        if response.value.err.is_some() {
-            // tracing::error!("error: {:?}", response.value.err);
-            continue;
-        }
+        // // Filter out responses containing errors.
+        // if response.value.err.is_some() {
+        //     // tracing::error!("error: {:?}", response.value.err);
+        //     continue;
+        // }
 
         dbg!(response.clone());
-        process_rpc_logs_response(response).await;
+        // process_rpc_logs_response(response).await;
     }
 
     Ok(())
@@ -53,15 +55,15 @@ async fn main() -> Result<()> {
 //     }
 // }
 
-async fn process_rpc_logs_response(response: Response<RpcLogsResponse>) {
-    let rpc_logs_response = response.value;
-    // dbg!(rpc_logs_response.logs.len());
-    for log in rpc_logs_response.logs {
-        if !log.contains(INITIALIZE_2) {
-            continue;
-        }
-        tracing::info!("log: {}", log);
-        let signature_str = &rpc_logs_response.signature;
-        tracing::info!("signature: {}", signature_str);
-    }
-}
+// async fn process_rpc_logs_response(response: Response<RpcLogsResponse>) {
+//     let rpc_logs_response = response.value;
+//     // dbg!(rpc_logs_response.logs.len());
+//     for log in rpc_logs_response.logs {
+//         if !log.contains(INITIALIZE_2) {
+//             continue;
+//         }
+//         tracing::info!("log: {}", log);
+//         let signature_str = &rpc_logs_response.signature;
+//         tracing::info!("signature: {}", signature_str);
+//     }
+// }
